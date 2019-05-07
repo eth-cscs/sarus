5f0eb7a0fd697f65c4eacc52441427508142109a
Alberto Madonna <amadonna@cscs.ch>
2019-05-07 11:57:25 +0200

    Fixed integration test

--- CI/src/integration_tests/test_image_with_non_ascii_characters.py
+++ CI/src/integration_tests/test_image_with_non_ascii_characters.py
@@ -1,11 +1,11 @@
+# -*- coding: utf-8 -*-
+#
 # Sarus
 #
 # Copyright (c) 2018-2019, ETH Zurich. All rights reserved.
 #
 # Please, refer to the LICENSE file in the root directory.
 # SPDX-License-Identifier: BSD-3-Clause
-
-# -*- coding: utf-8 -*-
 
 import unittest
 
