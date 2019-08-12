e938e425870cf3c10adfb3be907f043f5c4f500b
Kean Mariotti <kean.mariotti@cscs.ch>
2019-08-12 11:09:04 +0200

    fix code coverage

--- CI/utility_functions.bash
+++ CI/utility_functions.bash
@@ -1,5 +1,14 @@
 #! /bin/echo This file is meant to be sourced
 
+fail_on_error() {
+    local last_command_exit_code=$?
+    local message=$1
+    if [ ${last_command_exit_code} -ne 0 ]; then
+        echo "${message}"
+        exit 1
+    fi
+}
+
 change_uid_gid_of_docker_user() {
     local host_uid=$1
     local host_gid=$2
@@ -58,15 +67,17 @@ build_sarus_archive() {
     (cd ${prefix_dir}/bin && wget https://github.com/opencontainers/runc/releases/download/v1.0.0-rc8/runc.amd64 && chmod +x runc.amd64)
     (cd ${prefix_dir} && mkdir -p var/OCIBundleDir)
     (cd ${prefix_dir}/.. && tar cfz ../${archive_name} *)
+    echo "Successfully built archive"
 
     cd ${pwd_backup}
 }
 
 install_sarus_from_archive() {
     local root_dir=$1; shift
-    local archive=$(realpath $1); shift
+    local archive=$1; shift
     local pwd_backup=$(pwd)
 
+    echo "Installing Sarus from archive ${archive}"
     mkdir -p ${root_dir}
     cd ${root_dir}
     rm -rf * #remove any other installation
@@ -74,8 +85,78 @@ install_sarus_from_archive() {
     local sarus_version=$(ls)
     ln -s ${sarus_version} default
     ./default/configure_installation.sh
+    echo "Successfully installed Sarus"
 
     cd ${pwd_backup}
+}
+
+run_tests() {
+    local host_uid=$1; shift
+    local host_gid=$1; shift
+    local build_type=$1; shift
+    local security_checks=$1; shift
+    local build_dir=$1; shift
+
+    change_uid_gid_of_docker_user ${host_uid} ${host_gid}
+    cd ${build_dir}
+
+    sudo -u docker CTEST_OUTPUT_ON_FAILURE=1 ctest --exclude-regex AsRoot
+    fail_on_error "Unit tests as normal user failed"
+
+    CTEST_OUTPUT_ON_FAILURE=1 ctest --tests-regex AsRoot
+    fail_on_error "Unit tests as root user failed"
+
+    sudo -u docker /sarus-source/CI/run_documentation_build_test.sh
+    fail_on_error "Documentation build test failed"
+
+    install_sarus_from_archive /opt/sarus ${build_dir}/sarus.tar.gz
+    fail_on_error "Failed to install Sarus from archive"
+
+    sudo -u docker PYTHONPATH=/sarus-source/CI/src:$PYTHONPATH PATH=/opt/sarus/default/bin:$PATH CMAKE_INSTALL_PREFIX=/opt/sarus/default HOME=/home/docker nosetests -v /sarus-source/CI/src/integration_tests/test*.py \
+    fail_on_error "Python integration tests failed"
+
+    if [ ${build_type} = "Debug" ]; then
+        sudo -u docker mkdir ${build_dir}/gcov
+        cd ${build_dir}/gcov
+        sudo -u docker gcov --preserve-paths $(find ${build_dir}/src -name "*.gcno" |grep -v test |tr '\n' ' ')
+        sudo -u docker gcovr -r /sarus-source/src -k -g --object-directory ${build_dir}/gcov
+    fi
+}
+
+build_install_test_sarus() {
+    local build_type=$1; shift
+    local security_checks=$1; shift
+
+    local host_uid=$(id -u)
+    local host_gid=$(id -g)
+    local docker_image_build=ethcscs/sarus-ci-build:1.0.0
+    local docker_image_run=ethcscs/sarus-ci-run:1.0.0
+    local build_folder=build-${build_type}
+    local build_dir=/sarus-source/${build_folder}
+
+    if [ ${security_checks} = TRUE ]; then
+        build_folder=${build-folder}-WithSecurityChecks
+    fi
+
+    # Use cached build artifacts
+    test -e ~/cache/ids/sarus/openssh.tar && mkdir -p ${build_folder}/dep && cp ~/cache/ids/sarus/openssh.tar ${build_folder}/dep/openssh.tar
+    test -e ~/cache/ids/sarus/cpputest && mkdir -p ${build_folder}/dep && cp -r ~/cache/ids/sarus/cpputest ${build_folder}/dep/cpputest
+
+    # Build Sarus archive
+    docker run --tty --rm --user root --mount=src=$(pwd),dst=/sarus-source,type=bind ${docker_image_build} bash -c "
+        . /sarus-source/CI/utility_functions.bash \
+        && change_uid_gid_of_docker_user ${host_uid} ${host_gid} \
+        && sudo -u docker bash -c '. /sarus-source/CI/utility_functions.bash && build_sarus_archive ${build_type} ${security_checks}'"
+
+    # Run tests
+    local sarus_cached_home_dir=~/cache/ids/sarus/home_dir
+    local sarus_cached_centralized_repository_dir=~/cache/ids/sarus/centralized_repository_dir
+    docker run --tty --rm --user root --privileged \
+        --mount=src=$(pwd),dst=/sarus-source,type=bind \
+        --mount=src=${sarus_cached_home_dir},dst=/home/docker,type=bind \
+        --mount=src=${sarus_cached_centralized_repository_dir},dst=/var/sarus/centralized_repository,type=bind \
+        ${docker_image_run} \
+        bash -c ". /sarus-source/CI/utility_functions.bash && run_tests ${host_uid} ${host_gid} ${build_type} ${security_checks} ${build_dir}"
 }
 
 generate_slurm_conf() {
--- cmake/modules/add_unit_test.cmake
+++ cmake/modules/add_unit_test.cmake
@@ -3,19 +3,13 @@ include_directories(${CPPUTEST_INCLUDE_DIR})
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DCPPUTEST_MEM_LEAK_DETECTION_DISABLED")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DCPPUTEST_MEM_LEAK_DETECTION_DISABLED")
 
-function(add_unit_test test_name test_src_file uut_src_file link_libraries object_files_directory)
+function(add_unit_test test_name test_src_file link_libraries)
     set(test_bin_file test_${test_name})
     add_executable(${test_bin_file} ${test_src_file})
     target_link_libraries(${test_bin_file} ${link_libraries} ${CPPUTEST_LIB})
     add_dependencies(${test_bin_file} cpputest_library)
 
     set(test_commands "cd ${CMAKE_CURRENT_SOURCE_DIR} && ${CMAKE_CURRENT_BINARY_DIR}/${test_bin_file}")
-
-    if(${ENABLE_TESTS_WITH_GCOV})
-        if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/../${uut_src_file})
-            set(test_commands "${test_commands} && gcov -b ${uut_src_file} --object-directory ${object_files_directory}")
-        endif()
-    endif()
 
     if(${ENABLE_TESTS_WITH_VALGRIND})
         set(test_commands "${test_commands} && valgrind --tool=memcheck --leak-check=full --suppressions=valgrind.suppressions --error-exitcode=1 ${CMAKE_CURRENT_BINARY_DIR}/${test_bin_file}")
