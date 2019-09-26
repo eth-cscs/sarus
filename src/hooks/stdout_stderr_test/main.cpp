17b6b627cefde5bd2d437da5b5a64aad91a35cfd
Kean Mariotti <kean.mariotti@cscs.ch>
2019-09-26 12:15:35 +0200

    add "hook stdout stderr" integration test

---
+++ src/hooks/stdout_stderr_test/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Sarus
+ *
+ * Copyright (c) 2018-2019, ETH Zurich. All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include <iostream>
+
+#include "hooks/common/Utility.hpp"
+
+using namespace sarus;
+
+int main(int argc, char* argv[]) {
+    hooks::common::utility::useSarusStdoutStderrIfAvailable();
+
+    std::cout << "hook's stdout" << std::endl;
+    std::cerr << "hook's stderr" << std::endl;
+
+    return 0;
+}
