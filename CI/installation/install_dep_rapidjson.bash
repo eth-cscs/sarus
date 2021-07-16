b6a685ea767968d23fd251e86271da020070cff7
Alberto Madonna <amadonna@cscs.ch>
2021-07-16 17:22:21 +0000

    Miscellaneous fixes

--- CI/installation/install_dep_rapidjson.bash
+++ CI/installation/install_dep_rapidjson.bash
@@ -8,6 +8,6 @@ cd /tmp && \
     wget -O rapidjson-master.tar.gz https://github.com/Tencent/rapidjson/archive/663f076c7b44ce96526d1acfda3fa46971c8af31.tar.gz && \
     tar xvzf rapidjson-master.tar.gz && \
     cd rapidjson-663f076c7b44ce96526d1acfda3fa46971c8af31 && \
-    sudo mkdir -p /usr/local/include/rapidjson && sudo cp -r include/rapidjson /usr/local/include/rapidjson && \
+    sudo mkdir -p /usr/local/include/rapidjson && sudo cp -r include/rapidjson/* /usr/local/include/rapidjson/ && \
     cd ${pwd_bak} && \
     rm -rf /tmp/rapidjson
