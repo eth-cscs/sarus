daab6fac76ee6a9e65eff1249df29b9f89bad9c3
Alberto Madonna <amadonna@cscs.ch>
2022-10-26 14:25:44 +0200

    Bumped version to 1.5.2

--- spack/packages/sarus/package.py
+++ spack/packages/sarus/package.py
@@ -30,12 +30,13 @@ class Sarus(CMakePackage):
     """Sarus is an OCI-compliant container engine for HPC systems."""
 
     homepage = "https://github.com/eth-cscs/sarus"
-    url      = "https://github.com/eth-cscs/sarus/archive/1.5.1.tar.gz"
+    url      = "https://github.com/eth-cscs/sarus/archive/1.5.2.tar.gz"
     git      = "https://github.com/eth-cscs/sarus.git"
 
     version('develop', branch='develop', submodules=True)
     version('master',  branch='master', submodules=True)
-    version("1.5.1", tag="1.5.1", submodules=True)
+    version("1.5.2", tag="1.5.2", submodules=True)
+    version("1.5.1", commit="9808a4ab7f86359d77f574962f020839266b9ab8")
     version("1.5.0", commit="3e244ec8db85978aa42b1c5f47bc94846958bd96")
     version("1.4.2", commit="13af2723b5c1cd1890d056af715946596f7ff2c6")
     version("1.4.1", commit="a73f6ca9cafb768f3132cfcef8c826af34eeff94")
