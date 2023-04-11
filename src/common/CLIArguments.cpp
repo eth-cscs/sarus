983ab66d681e3f9714d3f756d1811281fed652f3
Michele Brambilla <michele.brambilla@cscs.ch>
2023-04-11 09:10:18 +0000

    Update copyright notice to include year 2023

--- src/common/CLIArguments.cpp
+++ src/common/CLIArguments.cpp
@@ -1,7 +1,7 @@
 /*
  * Sarus
  *
- * Copyright (c) 2018-2022, ETH Zurich. All rights reserved.
+ * Copyright (c) 2018-2023, ETH Zurich. All rights reserved.
  *
  * Please, refer to the LICENSE file in the root directory.
  * SPDX-License-Identifier: BSD-3-Clause
