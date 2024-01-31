3efa80252d781f31309e6613f760f81496dbadd2
Alberto Madonna <alberto.madonna@cscs.ch>
2024-01-31 14:37:31 +0000

    Use Python 3.9 for integration tests on Rocky 8

--- CI/installation/install_packages_rocky:8.sh
+++ CI/installation/install_packages_rocky:8.sh
@@ -8,7 +8,7 @@ sudo dnf config-manager --set-enabled powertools
 sudo dnf install -y gcc gcc-c++ glibc-static libstdc++-static git make cmake autoconf \
     diffutils findutils wget which procps squashfs-tools zlib-devel zlib-static \
     boost-devel skopeo runc tini-static \
-    python3 python3-pip python3-setuptools platform-python-devel
+    python39 python39-pip python39-setuptools platform-python-devel
 sudo dnf clean all
 sudo rm -rf /var/cache/dnf
 
